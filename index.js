ce19036618e50ff3b503a1ae9a7644bbf8447a73
EduardoHRVitorino <prometheus29_09@proton.me>
2024-12-02 17:58:14 -0300

    02/12/2024

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ elementoduvida.forEach(function(duvida){   //"forEach" = para cada elemento
     duvida.addEventListener('click', function (){
         duvida.classList.add('fundo-verde')
         duvida.classList.add('ativa')
+        
     })  
 })
